95e122180feb798d0b9e96af9ac2a42e3f89cef5
mvlinh <mvlinh>
2022-06-27 02:01:02 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,98 @@
+import firebase from 'firebase';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// firebase の初期化
+if (!firebase.apps.length) {
+ const firebaseConfig = {
+  apiKey: "AIzaSyCd9obA2pc6N2dO4fvBAZjcbAMRwm3DySQ",
+  authDomain: "todolist-itssk63.firebaseapp.com",
+  databaseURL: "https://todolist-itssk63-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "todolist-itssk63",
+  storageBucket: "todolist-itssk63.appspot.com",
+  messagingSenderId: "978530566137",
+  appId: "1:978530566137:web:e12e86a905a88882354728",
+  measurementId: "G-KXFK74F9X2"
+};
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+export const auth = firebase.auth();
+export default firebase;
+
+export const getFirebaseItems = async () => {
+  try {
+    const snapshot = await db
+      .collection("todos")
+      .get();
+    const items = snapshot.docs.map(
+      (doc) => ({ ...doc.data(), id: doc.id })
+    );
+    return items;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
+
+export const addFirebaseItem = async (item) => {
+  try {
+    const todoRef = db.collection("todos");
+    await todoRef.add(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const updateFirebaseItem = async (item, id) => {
+  try {
+    const todoRef = db.collection("todos").doc(id);
+    await todoRef.update(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const clearFirebaseItem = async (item) => {
+  const todoRef = db.collection("todos").doc(item.id);
+  await todoRef.delete().then(function () {
+  }).catch(function (err) {
+    console.log(err);
+  });
+};
+
+export const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: "/",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+}
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user;
+  const userDoc = await db.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await db.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    };
+  }
+}
+
+export const updateUser = async (user, image) => {
+  try {
+    const userDoc = await firebase.firestore().collection("users").doc(user.id).get();
+    if (userDoc.exists) {
+      await firebase.firestore().collection("users").doc(user.id).update({ ...userDoc.data(), image: image });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
